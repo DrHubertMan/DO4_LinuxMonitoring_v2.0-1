24cb46d9555981ad47f0de4f59f1c65c77521e49
mammiemi <mammiemi@student.21-school.ru>
2023-01-09 19:52:13 +0000

    fix

--- src/09/main.sh
+++ src/09/main.sh
@@ -2,10 +2,10 @@
 
 for ((i=1; i=1; ));
 do
-    >/var/www/project/stat.html
-    echo "$(cat /proc/stat | grep cpu | awk 'NR==1{print $0}')">>/var/www/project/stat.html
-    echo "$(free | awk 'NR==2{print $0}')">>/var/www/project/stat.html
-    echo "Disk Used - $(df / | awk 'NR==2{print $3}')" >>/var/www/project/stat.html
-    echo "Disk Available - $(df / | awk 'NR==2{print $4}')">>/var/www/project/stat.html
+    >/var/www/project/metrics.html
+    echo "$(cat /proc/stat | grep cpu | awk 'NR==1{print $0}')">>/var/www/project/metrics.html
+    echo "$(free | awk 'NR==2{print $0}')">>/var/www/project/metrics.html
+    echo "Disk Used - $(df / | awk 'NR==2{print $3}')" >>/var/www/project/metrics.html
+    echo "Disk Available - $(df / | awk 'NR==2{print $4}')">>/var/www/project/metrics.html
     sleep 3
 done
